e9e844ed6826be1344901dc1d14cd7cac98ec2d7
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-11-27 10:28:38 -0500

    navbar in progress

---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      { name: "Editor picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg" },
+      { name: "Newest", href: "#", imageSrc: "/nav/ui-kits/blue.jpg" },
+      { name: "Popular", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      { name: "Favorite Icon Picks", href: "#", imageSrc: "/nav/icons/picks.jpg" },
+      { name: "Newest Icons", href: "#", imageSrc: "/nav/icons/new.jpg" },
+      { name: "Best Sellers", href: "#", imageSrc: "/nav/icons/bestsellers.jpg" },
+    ],
+  },
+];
